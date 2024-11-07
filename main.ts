1d1a127ea83b1c9b2851e410272ee60779ee980e
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2024-11-07 15:31:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 * Refer to https://wiki.dfrobot.com/DFPlayer_Mini_SKU_DFR0299
 */
 
-//% weight=9 color=#666699 icon="\uf001" block="DFPlayer Mini"
+//% weight=9 color=#666699 icon="\uf001" block="Soundbox"
 namespace dfplayermini {
     /* [$S,VER,Len,CMD,Feedback,para1,para2,checksum,$0] */
     let dataArr: number[] = [0x7E, 0xFF, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xEF]
@@ -11,14 +11,19 @@ namespace dfplayermini {
 
     export enum playType {
         //% block="Play"
+        //% block.loc.de="abspielen"
         Play = 0x0D,
         //% block="Stop"
+        //% block.loc.de="Stop"
         Stop = 0x16,
         //% block="Play Next"
+        //% block.loc.de="nächsten abspielen"
         PlayNext = 0x01,
         //% block="Play Previous"
+        //% block.loc.de="vorherigen abspielen"
         PlayPrevious = 0x02,
         //% block="Pause"
+        //% block.loc.de="Pause"
         Pause = 0x0E
     }
 
@@ -39,8 +44,10 @@ namespace dfplayermini {
 
     export enum isRepeat {
         //% block="No"
+        //% block.loc.de="nein"
         No = 0,
         //% block="Yes"
+        //% block.loc.de="ja"
         Yes = 1
     }
 
@@ -79,7 +86,8 @@ namespace dfplayermini {
      * @param pinRX RX Pin, eg: SerialPin.P0
      * @param pinTX TX Pin, eg: SerialPin.P1
      */
-    //% blockId="dfplayermini_connect" block="connect to DFPlayer mini, RX:%pinRX|TX:%pinTX"
+    //% blockId="dfplayermini_connect" block="connect to DFPlayer mini, RX:%pinRX|TX:%pinTX" block.loc.de="Soundbox verbinden mit %pinRX|%pinTX"
+  
     //% weight=100 blockGap=20
     export function connect(pinRX: SerialPin = SerialPin.P0, pinTX: SerialPin = SerialPin.P1): void {
         serial.redirect(pinRX, pinTX, BaudRate.BaudRate9600)
